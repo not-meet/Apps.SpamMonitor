831a5bf46cc2e7fa8aaf3f5481debd13dfe30285
not-meet <mjmeet.279@gmail.com>
2026-08-01 21:38:31 +0530

    [fix from pr]: added changes made to the branch

--- src/constants/scheduleLogStore.ts
+++ src/constants/scheduleLogStore.ts
@@ -5,3 +5,5 @@ export const MAX_DAILY_ACTIONS = 25;
 // flag log store constants
 export const MAX_RECENT_EVENTS = 25;
 export const MAX_ROOMS_PER_SUMMARY = 20;
+export const MAX_REPORT_WINDOW_DAYS = 31;
+export const DAY_RECORD_RETENTION_DAYS = 90;
